27f43489218dcd363eed80af1743d764b27c0b4e
Andrew Taylor <champstarr@gmail.com>
2014-11-29 10:54:11 -1000

    changes to FlappyApp

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -4,21 +4,21 @@ var sass = require('gulp-sass');
 gulp.task('styles', function () {
   return gulp.src('./sass/*.scss')
   .pipe(sass())
-  .pipe(gulp.dest('./css'));
+  .pipe(gulp.dest('./public/css'));
 
 });
 
 gulp.task('watch_styles', function() {
-  gulp.watch('./sass/*.scss', ['styles']);
-  gulp.watch('*.html', notifyLiveReload);
-  gulp.watch('css/*.css', notifyLiveReload);
+  gulp.watch('./sass/**/*.scss', ['styles']);
+  gulp.watch('/public/*.html', notifyLiveReload);
+  gulp.watch('/public/css/*.css', notifyLiveReload);
 });
 
 gulp.task('express', function() {
   var express = require('express');
   var app = express();
   app.use(require('connect-livereload')({port: 35729}));
-  app.use(express.static(__dirname));
+  app.use(express.static(__dirname + '/public'));
   app.listen(4000);
 });
 
